b4f3401325de183592f1cb42c2a048eaf2a581ba
Your Name <you@example.com>
2024-03-24 02:59:13 +0700

    test2

--- Demo02/Terraform/main.tf
+++ Demo02/Terraform/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "<region>" # Thay thế <region> bằng khu vực mong muốn, ví dụ: us-east-1
+  region = "us-east-1" 
 }
 
 resource "aws_instance" "ec2_instance" {
-  ami           = "<ami_id>" # Thay thế <ami_id> bằng ID của AMI, ví dụ: ami-0c55b159cbfafe1f0
-  instance_type = "<instance_type>" # Thay thế <instance_type> bằng loại instance mong muốn, ví dụ: t2.micro
+  ami           = "ami-0c101f26f147fa7fd"
+  instance_type = "t2.micro"
   
   tags = {
     Name = "MyEC2Instance"

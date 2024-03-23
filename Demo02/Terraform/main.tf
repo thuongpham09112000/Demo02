d52608997274e0aac4df0fa9a3040d8ae0b505f9
Your Name <you@example.com>
2024-03-24 02:29:13 +0700

    Test 1

--- Demo02/Terraform/main.tf
+++ Demo02/Terraform/main.tf
@@ -1,12 +1,13 @@
 provider "aws" {
-  region = "us-east-1"  # Thay đổi khu vực nếu cần
+  region = "<region>" # Thay thế <region> bằng khu vực mong muốn, ví dụ: us-east-1
 }
 
 resource "aws_instance" "ec2_instance" {
-  ami           = "ami-0c55b159cbfafe1f0"  # AMI của Amazon Linux
-  instance_type = "t2.micro"
+  ami           = "<ami_id>" # Thay thế <ami_id> bằng ID của AMI, ví dụ: ami-0c55b159cbfafe1f0
+  instance_type = "<instance_type>" # Thay thế <instance_type> bằng loại instance mong muốn, ví dụ: t2.micro
   
   tags = {
     Name = "MyEC2Instance"
   }
 }
+
